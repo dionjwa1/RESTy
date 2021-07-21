a720cc7788d926f6c27fca59466e419c3e90ab67
dionjwa1 <dionjwa@gmail.com>
2021-07-20 19:03:13 -0700

    testing update

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import beautify from "json-beautify";
+
 function Results (props){
  
     return (
       <section>
-        <pre>{props.data ? beautify(props.data, undefined, 2) : null}</pre>
+        <input test data-testid='resultData' value={props.data ? beautify(props.data, undefined, 2) : null} />
       </section>
     );
   }
